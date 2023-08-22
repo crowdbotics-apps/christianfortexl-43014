90f805c94a5ab061d88b932911c43085707715cd
Crowdbotics <team@crowdbotics.com>
2023-08-22 03:42:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "christianfortexl-43014",
+  url: "https://christianfortexl-43014.botics.co",
+  api: "https://christianfortexl-43014.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
