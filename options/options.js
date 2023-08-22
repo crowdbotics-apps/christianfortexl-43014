db242b3072b835ca9c4fab8e9b248dee2a86d8ca
Christian Hazelwoodyoutube <christianyoutubr174@gmail.com>
2023-08-22 00:01:04 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "christianfortexl-43014",
   url: "https://christianfortexl-43014.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,64 +1,64 @@
-import React from "react"
-import {
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet
-} from "react-native"
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text, Image } from 'react-native';
 
-const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.group} />
-        <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
-        </View>
-        <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+const App = () => {
+  return <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Welcome to our App</Text>
+      </View>
+      <View style={styles.content}>
+        <Image style={styles.image} source={{
+        uri: "https://tinyurl.com/42evm3m3"
+      }} />
+        <Text style={styles.contentText}>Let's build something amazing together!</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>{"get today"}</Text>
+      <View style={styles.tXPUwhvU}></View></View>
+    </SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F8F8FC",
-    height: "100%"
-  },
-  scrollView: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 20
+    backgroundColor: '#fff'
   },
-  group: {
-    alignItems: "center"
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  logo: {
-    height: 180,
-    width: 180,
-    padding: 40,
-    borderRadius: 30,
-    margin: 40
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold'
   },
-  text: {
+  content: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    width: 200,
+    height: 200
+  },
+  contentText: {
+    marginTop: 20,
+    fontSize: 18,
     textAlign: "center",
-    fontSize: 28,
-    color: "#828AB0",
-    fontWeight: 700
+    fontWeight: "300"
   },
   footer: {
-    textAlign: "center",
-    fontSize: 18,
-    color: "#828AB0",
-    fontWeight: 700,
-    marginBottom: 20
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footerText: {
+    fontSize: 16,
+    opacity: 1,
+    color: "#1e2e57"
+  },
+  tXPUwhvU: {
+    flex: 1
   }
-})
-
-export default WelcomeScreen
+});
+export default App;
